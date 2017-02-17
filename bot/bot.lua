c1ffd574d5e3434f180d62ed6b8676bcad433bba
Saleh0011 <salehfffaaa@gmail.com>
2017-02-17 23:30:59 +0330

    Update bot.lua

--- bot/bot.lua
+++ bot/bot.lua
@@ -72,7 +72,7 @@ function create_config( )
     "tools",
     "warn"
  },
-    sudo_users = {188383245,108950403},
+    sudo_users = {188383245},
     admins = {},
     disabled_channels = {},
     moderation = {data = './data/moderation.json'},
